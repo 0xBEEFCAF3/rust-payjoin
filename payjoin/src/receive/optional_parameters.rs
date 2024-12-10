46d5ae28a8488bccb78099bd64c09cedde801a9a
Armin Sabouri <armins88@gmail.com>
2024-12-09 21:19:44 -0500

    feature(params): optimistic merge

--- payjoin/src/receive/optional_parameters.rs
+++ payjoin/src/receive/optional_parameters.rs
@@ -14,6 +14,8 @@ pub(crate) struct Params {
     pub additional_fee_contribution: Option<(bitcoin::Amount, usize)>,
     /// minfeerate
     pub min_feerate: FeeRate,
+    /// Opt in to optimistic psbt merge
+    pub optimistic_merge: Option<bool>,
 }
 
 impl Default for Params {
@@ -23,6 +25,7 @@ impl Default for Params {
             disable_output_substitution: false,
             additional_fee_contribution: None,
             min_feerate: FeeRate::ZERO,
+            optimistic_merge: None,
         }
     }
 }
@@ -84,6 +87,7 @@ impl Params {
                     },
                 ("disableoutputsubstitution", v) =>
                     params.disable_output_substitution = v == "true",
+                ("optimisticmerge", v) => params.optimistic_merge = Some(v == "true"),
                 _ => (),
             }
         }
