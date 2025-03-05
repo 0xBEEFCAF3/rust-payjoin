693804afe8b43e1c43c966f59f2acc699784160a
Armin Sabouri <armins88@gmail.com>
2025-03-05 13:27:19 -0500

    Persist sender when initialized
    This commit adds a new `Persister` parameter to `v2::Sender::new`,
    making it explicit that persistence is required when creating a
    sender. Applications can define how and in what format to persist the
    sender state by implementing the `Persistable` trait. The
    `Persistable` key for the sender is the directory ID associated with a
    particular session, aligning with the key used by Payjoin CLI as of this
    commit.
    
    For cases where persistence is not needed, such as testing, a no-op
    persister is available.
    
    Issue: https://github.com/payjoin/rust-payjoin/issues/336

--- payjoin-cli/src/db/v2.rs
+++ payjoin-cli/src/db/v2.rs
@@ -4,11 +4,41 @@ use bitcoincore_rpc::jsonrpc::serde_json;
 use payjoin::directory::ShortId;
 use payjoin::persist::Persister;
 use payjoin::receive::v2::Receiver;
-use payjoin::send::v2::Sender;
+use payjoin::send::v2::{Sender, UrlWrapper};
 use sled::{IVec, Tree};
 use url::Url;
 
 use super::*;
+
+#[derive(Clone)]
+pub(crate) struct SenderPersister(pub Arc<Database>);
+impl Persister<Sender> for SenderPersister {
+    type Error = crate::db::error::Error;
+    type Token = UrlWrapper;
+    fn save<K: Into<Vec<u8>>>(
+        &mut self,
+        key: K,
+        value: Sender,
+    ) -> std::result::Result<Self::Token, Self::Error> {
+        let send_tree = self.0 .0.open_tree("send_sessions")?;
+        let value = serde_json::to_string(&value).map_err(Error::Serialize)?;
+        let key_bytes = key.into();
+        send_tree.insert(key_bytes.as_slice(), IVec::from(value.as_str()))?;
+        send_tree.flush()?;
+        // TODO: remove unwrap
+        let url = UrlWrapper::try_from(key_bytes).unwrap();
+        Ok(url)
+    }
+
+    fn load(&self, token: &Self::Token) -> std::result::Result<Sender, Self::Error> {
+        let send_tree = self.0 .0.open_tree("send_sessions")?;
+        let key_bytes: Vec<u8> = token.0.to_string().into_bytes();
+        let value =
+            send_tree.get(key_bytes.as_slice())?.ok_or(Error::NotFound(token.0.to_string()))?;
+        let value = String::from_utf8(value.to_vec()).map_err(Error::FromUtf8)?;
+        serde_json::from_str(&value).map_err(Error::Deserialize)
+    }
+}
 
 #[derive(Clone)]
 pub(crate) struct ReceiverPersister(pub Arc<Database>);
@@ -51,14 +81,6 @@ impl Database {
         let recv_tree: Tree = self.0.open_tree("recv_sessions")?;
         recv_tree.clear()?;
         recv_tree.flush()?;
-        Ok(())
-    }
-
-    pub(crate) fn insert_send_session(&self, session: &mut Sender, pj_url: &Url) -> Result<()> {
-        let send_tree: Tree = self.0.open_tree("send_sessions")?;
-        let value = serde_json::to_string(session).map_err(Error::Serialize)?;
-        send_tree.insert(pj_url.to_string(), IVec::from(value.as_str()))?;
-        send_tree.flush()?;
         Ok(())
     }
 
