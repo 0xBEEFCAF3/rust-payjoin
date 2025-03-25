chaincase-app/payjoin

Introduce `Persistable` trait
This trait is meant to be impl'd by any pub payjoin type that is meant to be
persisted by the application. The trait has an associated `Key` type
which uniquely identifies the type. The trait imposes no specific
serialization format. The format depends on what the applications
intends to do with the data.

Issue: https://github.com/payjoin/rust-payjoin/issues/336

payjoin/src/lib.rs
@@ -25,6 +25,9 @@ pub mod receive;
 #[cfg(feature = "_core")]
 pub mod send;
 
+#[cfg(feature = "_core")]
+pub mod traits;
+
 #[cfg(feature = "v2")]
 pub(crate) mod hpke;
 #[cfg(feature = "v2")]

0xBEEFCAF3/rust-payjoin

payjoin/src/lib.rs
@@ -29,6 +29,9 @@ pub mod receive;
 pub mod send;
 
 #[cfg(feature = "v2")]
+pub mod persist;
+
+#[cfg(feature = "v2")]
 pub(crate) mod hpke;
 #[cfg(feature = "v2")]
 pub use crate::hpke::{HpkeKeyPair, HpkePublicKey};